40cf174833ef1ff4ec4463d917836e75637435ce
Yugo Omura <yomura@YugonoMacBook-puro.local>
2020-10-11 11:44:50 +0900

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if not config_path.exists():
 
 setup(
     name='pjmutil',
-    version='1.4.5',
+    version='1.4.6',
     description='',
     author='yomura',
     author_email='yomura@hoge.jp',
