ed0932868d87dc64f7232b0d4238d03c45db0c69
Yugo Omura <yomura@YugonoMacBook-puro.local>
2021-08-02 02:07:41 +0900

    modified:   pjmutil/scripts/pjm_kill.py 	modified:   setup.py

--- pjmutil/scripts/pjm_kill.py
+++ pjmutil/scripts/pjm_kill.py
@@ -11,13 +11,18 @@ def main():
     args = parser.parse_args()
     data = pjmutil.job_status.get_job_info(args.pattern[0])
 
-    if args.dry_run:
-        return 0
-
     if data.size == 0:
         raise ValueError(f"No process names matched")
 
-    print("\n".join(data["job_name"]))
+    print(
+        "\n".join([
+            "Running processes matched:",
+            *data["job_name"]
+        ])
+    )
+    if args.dry_run:
+        return 0
+
     yn = input("Are you sure that all running processes shown above will be killed? [y/n] ")
     if yn == "y":
         subprocess.run(["pjdel", " ".join(data["job_id"])])
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if not config_path.exists():
 
 setup(
     name='pjmutil',
-    version='4.3.1',
+    version='4.3.2',
     description='',
     author='yomura',
     author_email='yomura@hoge.jp',
