f8c5bf60d38419c6135c61555aebeade5f9cd1f4
Yugo Omura <yomura@YugonoMacBook-puro.local>
2021-08-02 20:57:44 +0900

    modified:   pjmutil/scripts/pjm_kill.py 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if not config_path.exists():
 
 setup(
     name='pjmutil',
-    version='4.3.4',
+    version='4.3.5',
     description='',
     author='yomura',
     author_email='yomura@hoge.jp',
